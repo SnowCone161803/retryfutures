72edbde95d1da79cbc3d0688692b68b64542b1ec
Gordon Macrae <macraegordon@hotmail.com>
2021-01-29 08:11:49 +0000

    updated with spring initializr project

---
+++ src/main/java/com/example/retryfutures/RetryfuturesApplication.java
@@ -0,0 +1,13 @@
+package com.example.retryfutures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryfuturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetryfuturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/retryfutures/RetryfuturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retryfutures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryfuturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
